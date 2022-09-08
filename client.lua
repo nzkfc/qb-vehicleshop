cb8de246c55486a889bc9e601a9978c9674ef32e
r0adra93 <github.com@legendsclan.org>
2022-09-08 15:47:31 -0400

    Removed Unneeded Logic Check
    The shop is already selecting the category.
    There is no real need for selecting the shop in the vehicles.lua.
    If the player wants the vehicle in another category all they have to do is create a new category and update their vehicle as required.

--- client.lua
+++ client.lua
@@ -505,7 +505,7 @@ RegisterNetEvent('qb-vehicleshop:client:openVehCats', function(data)
         }
     }
     for k, v in pairs(QBCore.Shared.Vehicles) do
-        if QBCore.Shared.Vehicles[k]["category"] == data.catName and QBCore.Shared.Vehicles[k]["shop"] == insideShop then
+        if QBCore.Shared.Vehicles[k]["category"] == data.catName then
             vehMenu[#vehMenu + 1] = {
                 header = v.name,
                 txt = Lang:t('menus.veh_price') .. v.price,
