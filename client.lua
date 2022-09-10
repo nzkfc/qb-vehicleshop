f134b1e5c6c00a3802ce878825879806c6945427
r0adra93 <github.com@legendsclan.org>
2022-09-10 14:07:11 -0400

    reading categories from variable
    Instead of listing categories for the store inside of vehiclestores.lua AND in the variable I modified vehicleshop/main.luua to pull the categories straight from the variable instead of listing the categories twice.

--- client.lua
+++ client.lua
@@ -470,6 +470,7 @@ RegisterNetEvent('qb-vehicleshop:client:TestDriveReturn', function()
 end)
 
 RegisterNetEvent('qb-vehicleshop:client:vehCategories', function()
+	local catmenu = {}
     local categoryMenu = {
         {
             header = Lang:t('menus.goback_header'),
@@ -479,7 +480,18 @@ RegisterNetEvent('qb-vehicleshop:client:vehCategories', function()
             }
         }
     }
-    for k, v in pairs(Config.Shops[insideShop]['Categories']) do
+	for k, v in pairs(QBCore.Shared.Vehicles) do
+        if type(QBCore.Shared.Vehicles[k]["shop"]) == 'table' then
+            for _, shop in pairs(QBCore.Shared.Vehicles[k]["shop"]) do
+                if shop == insideShop then
+                    catmenu[v] = true
+                end
+            end
+        elseif QBCore.Shared.Vehicles[k]["shop"] == insideShop then
+                catmenu[v] = v
+        end
+    end
+    for k, v in pairs(catmenu) do
         categoryMenu[#categoryMenu + 1] = {
             header = v,
             icon = "fa-solid fa-circle",
